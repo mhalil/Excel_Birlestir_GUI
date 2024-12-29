9cf8ca1bab39990fc7934e679062c604b597b2fb
Mustafa Halil <halil.mustafa@gmail.com>
2024-12-29 18:29:06 +0300

    Kontrol butonları entrylere baglandi. SS Yardım Düzenlendi.

--- birlestir.py
+++ birlestir.py
@@ -1,5 +1,5 @@
 from ttkbootstrap import Style
-from tkinter import ttk, filedialog, Tk, Toplevel, Label, PhotoImage
+from tkinter import ttk, filedialog, Tk, Toplevel, Label, PhotoImage, IntVar
 import glob
 import pandas as pd
 
@@ -51,6 +51,19 @@ def yardim():
 	etiket_resim = Label(yardim_penceresi, image=resim).pack()
 	yardim_penceresi.mainloop()
 
+
+def sayfa_adi_belirt():
+	if kontrol_sayfa_adi.get():
+		entry_sayfa_adi.config(state='normal')
+	else:
+		entry_sayfa_adi.config(state='disabled')
+
+def dosya_adi_belirt():
+	if kontrol_dosya_adi.get():
+		entry_kayit_dosya_adi.config(state='normal')
+	else:
+		entry_kayit_dosya_adi.config(state='disabled')
+
 ########### ARABİRİM OLUSTURULUYOR###########
 
 ### Klasör seçici
@@ -66,8 +79,12 @@ cerceve_parametreler = ttk.LabelFrame(
 cerceve_parametreler.grid(row=1, column=0, pady=5, padx=25, columnspan=2)
 
 ### Parametreler
-sayfa_belirt = ttk.Checkbutton(cerceve_parametreler, text='Sayfa Adı Belirt', style='primary.Roundtoggle.Toolbutton').grid(row=0, column=0, pady=5, padx=25)
-entry_sayfa_adi = ttk.Entry(cerceve_parametreler).grid(row=0, column=1, pady=5, padx=25)
+kontrol_sayfa_adi = IntVar()
+sayfa_belirt = ttk.Checkbutton(cerceve_parametreler, text='Sayfa Adı Belirt', style='primary.Roundtoggle.Toolbutton', variable=kontrol_sayfa_adi, command=sayfa_adi_belirt)
+sayfa_belirt.grid(row=0, column=0, pady=5, padx=25)
+
+entry_sayfa_adi = ttk.Entry(cerceve_parametreler, state='disabled')
+entry_sayfa_adi.grid(row=0, column=1, pady=5, padx=25)
 
 etiket_baslik_satir_no = ttk.Label(cerceve_parametreler, text='Başlık Satırı Numarası:').grid(row=1, column=0, pady=5, padx=25)
 entry_baslik_satiri = ttk.Entry(cerceve_parametreler).grid(row=1, column=1, pady=5, padx=25)
@@ -78,7 +95,7 @@ entry_ilk_veri_satiri = ttk.Entry(cerceve_parametreler).grid(row=2, column=1, pa
 etiket_kopyalanacak_satir = ttk.Label(cerceve_parametreler, text='Kopyalanacak Satır Sayısı:').grid(row=3, column=0, pady=5, padx=25)
 entry_kopyalanacak_satir = ttk.Entry(cerceve_parametreler).grid(row=3, column=1, pady=5, padx=25)
 
-etiket_atlanacak_satir = ttk.Label(cerceve_parametreler, text='Atlanacak Satırı Numarası:').grid(row=4, column=0, pady=5, padx=25)
+etiket_atlanacak_satir = ttk.Label(cerceve_parametreler, text='Atlanacak Satırı Sayısı:').grid(row=4, column=0, pady=5, padx=25)
 entry_atlanacak_satir = ttk.Entry(cerceve_parametreler).grid(row=4, column=1, pady=5, padx=25)
 
 etiket_kopyalanacak_sutun = ttk.Label(cerceve_parametreler, text='Kopyalanacak Sütunlar:').grid(row=5, column=0, pady=5, padx=25)
@@ -87,8 +104,10 @@ entry_kopyalanacak_sutun = ttk.Entry(cerceve_parametreler).grid(row=5, column=1,
 etiket_dongu_sayisi = ttk.Label(cerceve_parametreler, text='Döngü Sayısı:').grid(row=6, column=0, pady=5, padx=25)
 entry_dongu_sayisi = ttk.Entry(cerceve_parametreler).grid(row=6, column=1, pady=5, padx=25)
 
-kayit_dosya_adi = ttk.Checkbutton(cerceve_parametreler, text='Kayıt için Dosya Adı Belirt', style='primary.Roundtoggle.Toolbutton').grid(row=7, column=0, pady=5, padx=25)
-entry_kayit_dosya_adi = ttk.Entry(cerceve_parametreler).grid(row=7, column=1, pady=5, padx=25)
+kontrol_dosya_adi = IntVar()
+kayit_dosya_adi = ttk.Checkbutton(cerceve_parametreler, text='Kayıt için Dosya Adı Belirt', style='primary.Roundtoggle.Toolbutton', variable=kontrol_dosya_adi, command=dosya_adi_belirt).grid(row=7, column=0, pady=5, padx=25)
+entry_kayit_dosya_adi = ttk.Entry(cerceve_parametreler, state="disabled")
+entry_kayit_dosya_adi.grid(row=7, column=1, pady=5, padx=25)
 #####
 
 ### Alt Butonlar
